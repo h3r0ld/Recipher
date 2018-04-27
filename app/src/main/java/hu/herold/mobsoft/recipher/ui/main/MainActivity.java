f9c8319e8116f9251b1aa737062afed8eeb7694c
Herold Kristóf <kristof.herold@gmail.com>
2018-04-27 10:46:47 +0200

    Added material, created viewpager

--- app/src/main/java/hu/herold/mobsoft/recipher/ui/main/MainActivity.java
+++ app/src/main/java/hu/herold/mobsoft/recipher/ui/main/MainActivity.java
@@ -1,24 +1,48 @@
 package hu.herold.mobsoft.recipher.ui.main;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import hu.herold.mobsoft.recipher.R;
 import hu.herold.mobsoft.recipher.RecipherApplication;
+import hu.herold.mobsoft.recipher.ui.main.adapter.MainPagerAdapter;
 
 public class MainActivity extends AppCompatActivity implements MainScreen {
 
     @Inject
     MainPresenter mainPresenter;
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.tabs)
+    TabLayout tabLayout;
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+
+    private int[] tabIcons;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         RecipherApplication.injector.inject(this);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().hide();
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager.setAdapter(new MainPagerAdapter(this.getSupportFragmentManager()));
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
     }
 
     @Override
@@ -31,5 +55,25 @@ public class MainActivity extends AppCompatActivity implements MainScreen {
     protected void onStop() {
         super.onStop();
         mainPresenter.detachScreen();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (viewPager.getCurrentItem() == 0) {
+            // If the user is currently looking at the first step, allow the system to handle the
+            // Back button. This calls finish() on this activity and pops the back stack.
+            super.onBackPressed();
+        } else {
+            // Otherwise, select the previous step.
+            viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+        }
+    }
+
+    private void setupTabIcons() {
+        tabIcons = new int[] { R.drawable.ic_receipt_white_18dp, R.drawable.ic_stars_white_18dp, R.drawable.ic_info_white_18dp };
+
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
     }
 }
